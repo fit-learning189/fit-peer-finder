439b1a334383524ad7bd6b3ee0e4eec1d0a74da8
chuqudee-sand <cokereafor@sandtech.com>
2026-07-09 11:02:56 +0100

    Update config.js for FIT

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,6 +6,6 @@ const isProduction = window.location.hostname !== 'localhost' && window.location
 // 2. Set the API URL accordingly
 // REPLACE the string below with your actual Render Backend URL once you have it
 export const API_URL = isProduction 
-  ? 'https://alx-peerfinder.onrender.com' 
+  ? 'https://fit-peer-finder.onrender.com' 
 
   : 'http://127.0.0.1:5000';
